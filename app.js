893688e918c213902638d50c56d86c759c0b771e
Ryan Guinter <421guinterr@gmail.com>
2022-06-28 17:42:23 -0600

    changed endpoint to return everyting in database

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const knex = require('knex')(require('./knexfile.js')[process.env.NODE_ENV||'development']);
+
 require('dotenv').config()
 console.log(process.env)
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  
+  let result = await knex('first_table').select('*')
   res.status(200).json('something')
+
 })
 
 app.listen(port, () => console.log(`listening on port ${port}`))
